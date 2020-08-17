787b76c1c9d522d13db3e33b6a6a546fd9588fee
luismvargasg <1610@holbertonschool.com>
2020-08-17 22:40:47 +0000

    adding task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,5 +13,5 @@ echo "<html>
 </html>" >> /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "/server_name {/a\ \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 service nginx restart
