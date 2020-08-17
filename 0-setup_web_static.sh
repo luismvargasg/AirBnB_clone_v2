1f6b2c330c3eb1d6f135c5448884248b736c1ff0
luismvargasg <1610@holbertonschool.com>
2020-08-17 20:48:45 +0000

    adding task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static.
+apt-get update
+apt-get install nginx
+mkdir -p data/web_static/shared
+mkdir -p data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> data/web_static/releases/test/index.html
+ln -sf data/web_static/releases/test/ data/web_static/current
+chwon -R ubuntu:ubuntu data
+sed -i "19i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+service nginx restart
