774d6376c12b2b5a874fc916059d76291ffbee43
luismvargasg <1610@holbertonschool.com>
2020-09-01 05:01:06 +0000

    adding task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Method to display the welcome text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Method to display the welcome text"""
+    return 'HBNB'
+
+
+@app.route('/c/<input>', strict_slashes=False)
+def display_c_input(input):
+    """Method to display C + the input text"""
+    return 'C %s' % input.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<p_input>', strict_slashes=False)
+def display_python_input(p_input='is cool'):
+    """Method to display Python + the input text"""
+    return 'Python %s' % p_input.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Method to display "is a number" in case of input n being an int"""
+    return '%d is a number' % n
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
