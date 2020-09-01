8ab0a57cb0ff7f84a5f72a8ed58ddaedba7fa345
luismvargasg <1610@holbertonschool.com>
2020-09-01 04:09:33 +0000

    adding task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Method to display the welcome text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Method to display the welcome text"""
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
